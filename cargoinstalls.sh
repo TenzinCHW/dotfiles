f865e76d31138905aea9ae4b586ff0ce3799cb9a
Tenzin Chan <tenzin@Tenzins-MacBook-Air.local>
2023-03-17 13:44:43 +0800

    Added cargo installs

---
+++ cargoinstalls.sh
@@ -0,0 +1,24 @@
+# From Noboilerplate's video on YouTube
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source ~/.cargo/env
+cargo install sccache
+cargo install nu
+cargo install coreutils
+cargo install starship
+cargo install exa
+cargo install bat
+cargo install du-dust
+cargo install zellij
+cargo install mprocs
+cargo install ripgrep
+cargo install bob-nvim
+cargo install gitui
+cargo install irust
+cargo install evcxr_jupyter
+cargo install bacon
+cargo install cargo-info
+cargo install porsmo
+cargo install speedtest-rs
+cargo install wiki-tui
+cargo install rtx-cli
