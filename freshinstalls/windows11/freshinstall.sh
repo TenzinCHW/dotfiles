327e9ea1ddf32e4b5796c34a1a5fae0820df1953
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2025-02-27 19:00:17 +0800

    Updated komorebi install method

--- freshinstalls/windows11/freshinstall.sh
+++ freshinstalls/windows11/freshinstall.sh
@@ -6,10 +6,11 @@ cd ~/apps/vcpkg; ./bootstrap-vcpkg.sh
 winget.exe install Microsoft.PowerToys -s winget
 
 # Install komorebi
-git clone https://github.com/LGUG2Z/komorebi.git ~/apps/komorebi
-cd ~/apps/komorebi
-cargo +stable install --path komorebi --locked
-cargo +stable install --path komorebic --locked
-cargo +stable install --path komorebic-no-console --locked
-cargo +stable install --path komorebi-gui --locked
-cargo +stable install --path komorebi-bar --locked
+sudo add-apt-repository ppa:gnome3-team/gnome3 -y
+sudo add-apt-repository ppa:vala-team -y
+sudo add-apt-repository ppa:gnome3-team/gnome3-staging -y
+sudo apt install cmake valac libgtk-3-dev libgee-0.8-dev libclutter-gtk-1.0-dev libclutter-1.0-dev libwebkit2gtk-4.0-dev libclutter-gst-3.0-dev
+git clone https://github.com/cheesecakeufo/komorebi.git
+cd komorebi
+mkdir build && cd build
+cmake .. && sudo make install && ./komorebi
