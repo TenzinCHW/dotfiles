9cbbb1b0225f724742ce1e366a7a57b9e02b1e70
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2025-02-28 17:12:57 +0800

    Added script for installing nvidia drivers (12.8) on windows 10

---
+++ freshinstalls/windows10/nvidiadrivers.sh
@@ -0,0 +1,9 @@
+# Install nvidia drivers 12.8, instructions from https://docs.nvidia.com/cuda/wsl-user-guide/index.html#getting-started-with-cuda-on-wsl-2
+sudo apt-key rm 7fa2af80
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.ping /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-wsl-ubuntu-12-8-local_12.8.0-1_amd64.deb
+sudo dpkg-i cuda-repo-wsl-ubuntu-12-8-local_12.8.0-1_amd64.deb
+sudo cp /var/cuda-repo-wsl-ubuntu-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings
+sudo apt-get update
+sudo apt-get install -y cuda-toolkit-12-8
